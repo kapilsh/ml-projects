1befd70d57335fe6a31c78350bff7d29bf80247a
Kapil Sharma <kapilsharma.frm@gmail.com>
2024-07-17 15:56:04 -0400

    Implemented GPT2 model

---
+++ transformers/gpt.py
@@ -0,0 +1,98 @@
+import math
+from dataclasses import dataclass
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+@dataclass
+class GPT2Config:
+    vocab_size: int = 50257
+    context_window_size: int = 1024
+    embedding_dimension: int = 768
+    num_layers: int = 12
+    num_heads: int = 12
+    dropout_embeddings: float = 0.1
+    dropout_attention: float = 0.1
+    dropout_residual: float = 0.1
+    layer_norm_epsilon: float = 1e-5
+
+
+class GPT2(nn.Module):
+    def __init__(self, config: GPT2Config, device: str = 'cpu'):
+        super().__init__()
+        self.config = config
+        self.token_embedding = nn.Embedding(config.vocab_size, config.embedding_dimension)
+        self.positional_embedding = nn.Embedding(config.context_window_size, config.embedding_dimension)
+        self.positional_ids = torch.arange(config.context_window_size).unsqueeze(0).to(device)
+        self.embedding_dropout = nn.Dropout(config.dropout_embeddings)
+        self.layers = nn.ModuleList([GPT2Layer(config) for _ in range(config.num_layers)])
+        self.layer_norm = nn.LayerNorm(config.embedding_dimension)
+        # gpt2 does not use a bias in the output layer
+        self.output_layer = nn.Linear(config.embedding_dimension, config.vocab_size, bias=False)
+
+    def forward(self, input_ids: torch.Tensor):
+        _, current_sequence_length = input_ids.size()
+        positions = self.positional_ids[:, :current_sequence_length]
+        embeddings = self.token_embedding(input_ids) + self.positional_embedding(positions)
+        embeddings = self.embedding_dropout(embeddings)
+        for layer in self.layers:
+            embeddings = layer(embeddings)
+        embeddings = self.layer_norm(embeddings)
+        return self.output_layer(embeddings)  # returns logits
+
+
+class GPT2Layer(nn.Module):
+    def __init__(self, config: GPT2Config):
+        super().__init__()
+        self.layer_norm1 = nn.LayerNorm(config.embedding_dimension)
+        self.attention = CausalMultiHeadAttention(config)
+        self.dropout_attention = nn.Dropout(config.dropout_attention)
+        self.layer_norm2 = nn.LayerNorm(config.embedding_dimension)
+        self.mlp = MLP(config)
+        self.dropout_mlp = nn.Dropout(config.dropout_residual)
+
+    def forward(self, embeddings: torch.Tensor):
+        attention_output = embeddings + self.attention(self.layer_norm1(embeddings))
+        mlp_output = attention_output + self.mlp(self.layer_norm2(attention_output))
+        return mlp_output
+
+
+class MLP(nn.Module):
+    def __init__(self, config: GPT2Config):
+        super().__init__()
+        self.fc1 = nn.Linear(config.embedding_dimension, 4 * config.embedding_dimension)
+        self.activation = nn.GELU()
+        self.fc2 = nn.Linear(4 * config.embedding_dimension, config.embedding_dimension)
+
+    def forward(self, x: torch.Tensor):
+        return self.fc2(self.activation(self.fc1(x)))
+
+
+class CausalMultiHeadAttention(nn.Module):
+    def __init__(self, config: GPT2Config):
+        super().__init__()
+        self.num_heads = config.num_heads
+        self.embedding_dimension = config.embedding_dimension
+        self.head_dim = self.embedding_dimension // self.num_heads
+        assert self.head_dim * self.num_heads == self.embedding_dimension, \
+            "embedding_dimension must be divisible by num_heads"
+        self.qkv = nn.Linear(self.embedding_dimension, 3 * self.embedding_dimension)
+        self.out = nn.Linear(self.embedding_dimension, self.embedding_dimension)
+        self.prob_dropout = config.dropout_attention
+
+    def forward(self, x: torch.Tensor):
+        batch_size, sequence_length, _ = x.size()
+        qkv = self.qkv(x)
+        q, k, v = qkv.split(self.embedding_dimension, dim=2)  # split along the third dimension
+
+        k = k.view(batch_size, sequence_length, self.num_heads, self.head_dim).permute(0, 2, 1, 3)
+        v = v.view(batch_size, sequence_length, self.num_heads, self.head_dim).permute(0, 2, 1, 3)
+        q = q.view(batch_size, sequence_length, self.num_heads, self.head_dim).permute(0, 2, 1, 3)
+
+        weights = F.scaled_dot_product_attention(q, k, v, is_causal=True, dropout_p=self.prob_dropout)
+        output = weights.transpose(1, 2).contiguous().view(
+            batch_size, sequence_length, self.embedding_dimension)
+        output = self.out(output)
+        return output
